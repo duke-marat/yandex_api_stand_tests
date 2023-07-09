8f4684000ef33e657a8c84a53d3d5fc8af49bf33
marat-duke <dark.dragon13.mg@gmail.com>
2023-07-09 22:23:19 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://e667f6d9-bbb1-4b67-908b-7824d28be8df.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
